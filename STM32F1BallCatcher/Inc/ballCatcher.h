e4f0e47799fd43e84ef2496fa7dc47fac2718ece
Sebasthian Ogalde <saogalde@seba-mbpro.local>
2018-06-15 01:50:51 -0400

    First STM32F103C8T6 port of STMBallCatcher.

---
+++ STM32F1BallCatcher/Inc/ballCatcher.h
@@ -0,0 +1,77 @@
+/*
+ * ballCatcher.h
+ *
+ *  Created on: May 9, 2018
+ *      Author: saogalde
+ */
+
+#ifndef BALLCATCHER_H_
+#define BALLCATCHER_H_
+
+#include "stepperControl.h"
+#include <stdbool.h>
+
+#define DEFAULT_MICROSTEP		MICROSTEP_16
+#define STEPS_BY_TURN			(float)(200*16)
+#define PULLEY_RADIUS			2.5		// in cm
+#define PULLEY_PERIMETER		(float)(2.0*3.14f*PULLEY_RADIUS)
+#define CAPTURE_SIZE_CM_X		59.0F		// in cm
+#define CAPTURE_SIZE_CM_Y		42.0F		// in cm
+#define CORRECTION_X			1.4F
+#define CORRECTION_Y			1.4F
+#define CAPTURE_SIZE_STEPS_X	(int)((CAPTURE_SIZE_CM_X/PULLEY_PERIMETER)*STEPS_BY_TURN*CORRECTION_X)	// in 1*steps
+#define CAPTURE_SIZE_STEPS_Y	(int)((CAPTURE_SIZE_CM_Y/PULLEY_PERIMETER)*STEPS_BY_TURN*CORRECTION_Y) 	// in 1*steps
+
+typedef enum{
+	MODE_CALIB 			= 0x01,
+	MODE_WAITING		= 0x02,
+	MODE_CAPTURE		= 0x04,
+	MODE_POSTCAPTURE	= 0x08
+} ballcatcher_mode;
+
+typedef struct{
+	uint8_t x;
+	uint8_t y;
+} requested_position;
+
+requested_position req_pos;
+requested_position req_cat;
+ballcatcher_mode mode;
+
+stepper motorEjeCentral;
+stepper motorEjeBasal;
+TIM_HandleTypeDef* ballCatcherTimer;
+TIM_HandleTypeDef* stepperUpdateTimer;
+//TIM_HandleTypeDef* servoTimer;
+I2C_HandleTypeDef* hi2c;
+UART_HandleTypeDef* uartDevice;
+//SPI_HandleTypeDef* spiDevice;
+DMA_HandleTypeDef* hdma;
+volatile bool captured;
+volatile bool ballDetected;
+uint8_t rxBuffer;
+volatile bool receiving_pos;
+volatile bool receiving_cat;
+volatile bool x_otherwise_y;
+// initializes full system
+void initBallCatcher(TIM_HandleTypeDef* htimBasal, TIM_HandleTypeDef* htimCentral, \
+		TIM_HandleTypeDef* stUp, TIM_HandleTypeDef* bcTim, \
+		I2C_HandleTypeDef* i2cdevice, UART_HandleTypeDef* ud,
+		DMA_HandleTypeDef* dmad);
+void BallCatcher_Controller(stepper* st1, stepper* st2);
+void BallCatcher_TurnOnTimerIT();
+void BallCatcher_TurnOffTimerIT();
+void BallCatcher_TurnOnSerialIT();
+void BallCatcher_TurnOffSerialIT();
+void BallCatcher_SetTargetPosition(requested_position pos);
+void BallCatcher_TurnOffMotorsIT();
+void BallCatcher_TurnOnMotorsIT();
+void BallCatcher_OpenDoor();
+void BallCatcher_CloseDoor();
+
+// calibrates the system by going home and setting the 0,0 coordinate
+void calibrate();
+void motor_debug();
+// serial functionality
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
+#endif /* BALLCATCHER_H_ */
---
+++ STM32F1BallCatcher/Inc/stepperControl.h
@@ -0,0 +1,100 @@
+/*
+
+ * stepperControl.h
+ *
+ *  Created on: May 9, 2018
+ *      Author: saogalde
+ *      Based on https://github.com/omuzychko/StepperHub
+ */
+
+#ifndef STEPPERCONTROL_H_
+#define STEPPERCONTROL_H_
+
+#include "stdint.h"
+#include "stm32f1xx_hal.h"
+#include <stdbool.h>
+
+typedef enum{
+	MICROSTEP_1 		= 0x00,
+	MICROSTEP_2 		= 0x01,
+	MICROSTEP_4 		= 0x02,
+	MICROSTEP_8 		= 0x03,
+	MICROSTEP_16		= 0x04,
+	MICROSTEP_32		= 0x05,
+} stepper_microstep;
+
+typedef enum {
+    SS_UNDEFINED         = 0x00,
+    SS_RUNNING_BACKWARD  = 0x01,
+    SS_RUNNING_FORWARD   = 0x02,
+    SS_STARTING          = 0x04,
+    SS_BRAKING          = 0x10,
+    SS_BRAKECORRECTION   = 0x20,
+    SS_STOPPED           = 0x80
+} stepper_status;
+
+/*
+typedef enum {
+    SERR_OK                     = 0,
+    SERR_NOMORESTATESAVAILABLE  = 1,
+    SERR_MUSTBESTOPPED          = 2,
+    SERR_STATENOTFOUND          = 3,
+    SERR_LIMIT                  = 4
+} stepper_error;*/
+
+typedef struct{
+	GPIO_TypeDef* PIN_PORT;
+	uint16_t PIN_NO;
+} stepper_pin;
+
+typedef struct{
+	stepper_status status;
+	stepper_microstep microstepping;
+	bool calib;
+
+	// positions
+	volatile int32_t initialPosition;
+	volatile int32_t currentPosition;
+	volatile int32_t targetPosition;
+	uint32_t maxPosition;
+
+	// speeds
+	volatile int32_t minStepsSec;
+	volatile int32_t maxStepsSec;
+	volatile int32_t currentStepsSec;
+	uint32_t speedSpan;
+
+	// accelerations
+	volatile int32_t accelStepsSec;
+	volatile int32_t accelPrescaler;
+	volatile int32_t proportionalTerm;
+	//volatile int32_t accelStepsSec;
+
+	// associated hardware
+	TIM_HandleTypeDef* STEP_TIMER;
+	uint16_t STEP_CHANNEL;
+	stepper_pin STPIN_M0;
+	stepper_pin STPIN_M1;
+	stepper_pin STPIN_M2;
+	stepper_pin STPIN_EN;
+	stepper_pin STPIN_DIR;
+} stepper;
+
+void writeStepperPin(stepper_pin pin, uint8_t state);
+void Stepper_ChangeMicrostepping(stepper* st,stepper_microstep);
+void Stepper_InitializeHardware(stepper* st, TIM_HandleTypeDef* htim, uint8_t chtim,\
+		GPIO_TypeDef* DIRPort,uint16_t DIR, \
+		GPIO_TypeDef* ENPort,uint16_t EN);
+
+int32_t GetStepDirectionUnit(stepper* st);
+void Stepper_SetDefaultConfig(stepper* st, stepper_microstep ms);
+void Stepper_Enable(stepper* st);
+void Stepper_Disable(stepper* st);
+void Stepper_TurnOnTimer_IT(stepper* st);
+void Stepper_TurnOffTimer_IT(stepper* st);
+void Stepper_Controller(stepper* st);
+void Stepper_PulseUpdate(stepper* st);
+bool Stepper_AtTarget(stepper* st);
+int32_t Stepper_RemainingSteps(stepper* st);
+void watcher(stepper* st1,stepper* st2);
+#endif /* STEPPERCONTROL_H_ */
